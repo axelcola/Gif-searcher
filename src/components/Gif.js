0f7ba5718301bec9f414f79cca3e7437b6c680e0
Axel Cola <axelcola1@gmail.com>
2022-04-07 11:57:51 -0300

    styling ListOfGifs

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Gif.css";
-import { Link, Route } from "wouter";
-import SingleGif from "./SingleGif";
+import { Link } from "wouter";
 
 export default function Gif({ title, id, url }) {
   return (
     <div className="Gif">
-      <Link to={`/singlegif/${id}`} className="Gif-link">
-        <img src={url} alt={title} />
-        <h4>{title}</h4>
-      </Link>
+      <div className="Gif-link">
+        <Link to={`/singlegif/${id}`} className="Gif-link">
+          <img src={url} alt={title} />
+          <h4>{title}</h4>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Gif from "./Gif";
+import "./ListOfGifs.css";
 
 export default function ListOfGifs({ gifs }) {
   return (
-    <div>
+    <div className="ListOfGifs">
       {gifs.map(({ id, title, url }) => (
         <Gif id={id} key={id} title={title} url={url} />
       ))}
